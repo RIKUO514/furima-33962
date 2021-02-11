5f109f6f358b65daa8a4a77e5d56d97e3acace77
RIKUO514 <r19960514@gmail.com>
2021-02-11 14:20:10 +0900

    Rspecインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         
+
   validates :nikename,        presence: true
+  validates :password,        presence: true
   validates :fist_name,       presence: true
   validates :list_name,       presence: true 
   validates :fist_name_kana,  presence: true  
