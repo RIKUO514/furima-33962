1c5b744c60810872adf33364af4403b612c06ee5
RIKUO514 <r19960514@gmail.com>
2021-02-16 14:47:13 +0900

    単体テストコード/java入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,14 @@ class Item < ApplicationRecord
   with_options presence: true do
   validates :name
   validates :description
-  validates :price
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
   validates :user
+  validates :category_id
+  validates :information_id
+  validates :shipping_fee_id
+  validates :area_id
+  validates :days_id
+  validates :image
   end
 
   with_options numericality: { other_than: 1 }  do
@@ -19,6 +25,7 @@ class Item < ApplicationRecord
   validates :days_id
   end
 
+  
   
   
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name              {'test'}
+    description       {'test'}
+    category_id       {1}
+    information_id    {1}
+    shipping_fee_id   {1}
+    area_id           {1}
+    days_id           {1}
+    price             {'300'}
+
+   after(:build) do |message|
+    message.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+   end
   end
 end
